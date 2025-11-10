fbc5ffed0edc38147effca466e0cc684f235e660
Manasi170806 <manasipatel17082006@gmail.com>
2025-11-10 22:28:13 +0530

    done

---
+++ Jewelry Static Website/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from './components/HeroSections.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection />
+     
+     
+    </div>
+  )
+}
+
+export default App
